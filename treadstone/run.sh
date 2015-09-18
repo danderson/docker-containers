85cccd1e56c82b602929c7d011986aca6700ca5a
David Anderson <dave@natulte.net>
2015-09-18 16:44:48 -0700

    Enable connections to EXPOSEd ports of treadstone containers.
    This uses neat policy routing and connmarking tricks to bypass the VPN
    only for specific flows that were initiated from outside the container.

--- treadstone/run.sh
+++ treadstone/run.sh
@@ -7,13 +7,30 @@ nameserver 8.8.8.8
 nameserver 8.8.4.4
 EOF
 
+# Replicate the non-VPN routing table to a private table used only by
+# marked packets. Netfilter marks inbound connections to EXPOSEd ports
+# with this mark, and this routing table makes the responses bypass
+# the VPN.
+ip route show | while read -r line; do
+    ip route add table 100 $line
+done
+ip rule add fwmark 100 pref 100 table 100
+
 iptables-restore </treadstone/iptables.conf
 # IPv6 may not be around, depending on how docker is configured.
 #ip6tables-restore </treadstone/ip6tables.conf || true
 
+for portspec in $TREADSTONE_EXPOSE_PORTS; do
+    proto=$(echo $portspec | cut -f1 -d'/')
+    port=$(echo $portspec | cut -f2 -d'/')
+    set -x
+    iptables -A EXPOSED_PORTS -p $proto --dport $port -j CONNMARK --set-mark 100
+    set +x
+done
+
 ip tuntap add dev vpn mode tun user vpn
 
-su -c "openvpn --config ${VPN_CONFIG_PATH} --iproute /treadstone/iproute2.sh --route-noexec --route-up /treadstone/route-up.sh --script-security 2 --port 1194" vpn &
+su -c "openvpn --config ${TREADSTONE_VPN_CONFIG} --iproute /treadstone/iproute2.sh --route-noexec --route-up /treadstone/route-up.sh --script-security 2 --port 1194" vpn &
 
 while [ ! -f /tmp/vpn-up ]; do
     sleep 1
